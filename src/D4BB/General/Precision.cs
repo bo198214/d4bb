673519b59b504b194903fa994838c2dba0af3244
bo198214 <bo198214@gmail.com>
2024-02-01 21:49:32 +0100

    added some more precision code

--- src/D4BB/General/Precision.cs
+++ src/D4BB/General/Precision.cs
@@ -1,25 +1,105 @@
+
+using System;
+
 public static class Precision {
+    //https://stackoverflow.com/questions/14285492/efficient-way-to-round-double-precision-numbers-to-a-lower-precision-given-in-nu
+    /// <summary>
+    /// Round numbers to a specified number of significant binary digits.
+    /// 
+    /// For example, to 3 places, numbers from zero to seven are unchanged, because they only require 3 binary digits,
+    /// but larger numbers lose precision:
+    /// 
+    ///      8    1000 => 1000   8
+    ///      9    1001 => 1010  10
+    ///     10    1010 => 1010  10
+    ///     11    1011 => 1100  12
+    ///     12    1100 => 1100  12
+    ///     13    1101 => 1110  14
+    ///     14    1110 => 1110  14
+    ///     15    1111 =>10000  16
+    ///     16   10000 =>10000  16
+    ///     
+    /// This is different from rounding in that we are specifying the place where rounding occurs as the distance to the right
+    /// in binary digits from the highest bit set, not the distance to the left from the zero bit.
+    /// </summary>
+    /// <param name="d">Number to be rounded.</param>
+    /// <param name="digits">Number of binary digits of precision to preserve. </param>
+    // public static double AdjustPrecision(this double d, int digits)
+    // {
+    //     // TODO: Not sure if this will work for both normalized and denormalized doubles. Needs more research.
+    //     var shift = 53 - digits; // IEEE 754 doubles have 53 bits of significand, but one bit is "implied" and not stored.
+    //     ulong significandMask = (0xffffffffffffffffUL >> shift) << shift;
+    //     var local_d = d;
+    //     unsafe
+    //     {
+    //         // double -> fixed point (sorta)
+    //         ulong toLong = *(ulong*)(&local_d);
+    //         // mask off your least-sig bits
+    //         var modLong = toLong & significandMask;
+    //         // fixed point -> float (sorta)
+    //         local_d = *(double*)(&modLong);
+    //     }
+    //     return local_d;
+    // }
     private static double[] PowersOfTwoPlusOne;
+    private static double[] PowersOfTwo;
 
     static Precision()
     {
         PowersOfTwoPlusOne = new double[54];
-        for (var i = 0; i < PowersOfTwoPlusOne.Length; i++)
+        PowersOfTwo = new double[54];
+        PowersOfTwo[0] = 1;
+        PowersOfTwoPlusOne[0] = 1; // Special case.
+        for (var i = 1; i < PowersOfTwoPlusOne.Length; i++)
         {
-            if (i == 0)
-                PowersOfTwoPlusOne[i] = 1; // Special case.
-            else
-            {
-                long two_to_i_plus_one = (1L << i) + 1L;
-                PowersOfTwoPlusOne[i] = (double)two_to_i_plus_one;
-            }
+            long two_to_i = 1L << i;
+            PowersOfTwo[i] = (double)two_to_i;
+            PowersOfTwoPlusOne[i] = (double)(two_to_i + 1L);
         }
     }
-
     public static double RoundBinary(this double d, int binaryDigits)
     {
         double t = d * PowersOfTwoPlusOne[53 - binaryDigits];
         double rounded = t - (t - d);
         return rounded;
+    }
+    public static void mantissaExponent(double d, out long mantissa, out int exponent)
+    {
+        // Translate the double into sign, exponent and mantissa.
+        long bits = BitConverter.DoubleToInt64Bits(d);
+        // Note that the shift is sign-extended, hence the test against -1 not 1
+        bool negative = (bits & (1L << 63)) != 0;
+        exponent = (int) ((bits >> 52) & 0x7ffL);
+        mantissa = bits & 0xfffffffffffffL;
+
+        // Subnormal numbers; exponent is effectively one higher,
+        // but there's no extra normalisation bit in the mantissa
+        if (exponent==0)
+        {
+            exponent++;
+        }
+        // Normal numbers; leave exponent as it is but add extra
+        // bit to the front of the mantissa
+        else
+        {
+            mantissa = mantissa | (1L << 52);
+        }
+
+        // Bias the exponent. It's actually biased by 1023, but we're
+        // treating the mantissa as m.0 rather than 0.m, so we need
+        // to subtract another 52 from it.
+        exponent -= 1075;
+
+        if (mantissa == 0) 
+        {
+            return;
+        }
+
+        /* Normalize */
+        while((mantissa & 1) == 0) 
+        {    /*  i.e., Mantissa is even */
+            mantissa >>= 1;
+            exponent++;
+        }
     }
 }
